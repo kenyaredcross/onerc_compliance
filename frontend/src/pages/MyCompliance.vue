6707c89f3513f7389c140e67b9f6cb1dde817ae9
Njenga Sheba <njengasheba@gmail.com>
2026-06-17 10:46:08 +0300

    refactor/ changed 'Reference Document' to 'Reference Link'

--- frontend/src/pages/MyCompliance.vue
+++ frontend/src/pages/MyCompliance.vue
@@ -75,7 +75,7 @@
               <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/>
               </svg>
-              Reference Document
+              Reference Link/ Platform
             </a>
 
             <!-- Reviewer remark (Needs More Info) -->
